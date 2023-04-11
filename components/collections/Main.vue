dffae5a8f0b811a308656054bb94795409aaba61
vamsi-500apps <vamsi.ajjumpudi@500apps.com>
2023-04-11 16:14:21 +0530

    added props

--- components/collections/Main.vue
+++ components/collections/Main.vue
@@ -1,21 +1,18 @@
 <template>
   <div>
     <ClientOnly>
-        <!-- maps imported here -->
+      <!-- maps imported here -->
       <CollectionsMaps
         :url="url"
         :zoom="zoom"
         :center="center"
-        :markerLating="markerLatLng"
-        :attribution="attribution"
+        :latlng="markerLatLng"
     /></ClientOnly>
   </div>
 </template>
 <script setup lang="ts">
 const url = ref("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
-const attribution = ref(
-  '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-);
+
 const zoom = ref(15);
 const center = ref([51.505, -0.159]);
 const markerLatLng = ref([51.504, -0.159]);
